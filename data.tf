12caeb9a83e2f4580272019a88095c1ae215aef7
gaurav1996b <1996gauravb@gmail.com>
2022-08-02 21:05:59 +0530

    updating terraform file with roboshop project

--- data.tf
+++ data.tf
@@ -7,15 +7,15 @@ data "aws_secretsmanager_secret_version" "secret" {
 }
 
 data "aws_ami" "ami" {
-  executable_users = ["self"]
-  most_recent      = true
-  name_regex       = "base-with-ansible"
-  owners           = ["self"]
+  //executable_users = ["self"]
+  most_recent = true
+  name_regex  = "base-with-ansible"
+  owners      = ["self"]
 }
 
 data "aws_ec2_spot_price" "spot_price" {
   instance_type     = var.INSTANCE_TYPE
-  availability_zone = "us-west-2a"
+  availability_zone = data.aws_subnet.selected.availability_zone
 
   filter {
     name   = "product-description"
