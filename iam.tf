c82f07d6f3de984181daf8989cd184ddeb6e61a8
gaurav1996b <1996gauravb@gmail.com>
2022-08-03 12:50:26 +0530

    updating terraform file with roboshop project

---
+++ iam.tf
@@ -0,0 +1,67 @@
+resource "aws_iam_policy" "allow-secret-manager-read-access" {
+  name        = "Roboshop-RabbitMQ-SecretManager-ReadAccess-${var.ENV}"
+  path        = "/"
+  description = "Roboshop-RabbitMQ-SecretManager-ReadAccess-${var.ENV}"
+
+  # Terraform's "jsonencode" function converts a
+  # Terraform expression result to valid JSON syntax.
+  policy = jsonencode({
+    "Version" : "2012-10-17",
+    "Statement" : [
+      {
+        "Sid" : "VisualEditor0",
+        "Effect" : "Allow",
+        "Action" : [
+          "secretsmanager:GetResourcePolicy",
+          "secretsmanager:GetSecretValue",
+          "secretsmanager:DescribeSecret",
+          "secretsmanager:ListSecretVersionIds"
+        ],
+        "Resource" : "arn:aws:secretsmanager:us-east-1:504729315886:secret:roboshop/all-rcLqko"
+      },
+      {
+        "Sid" : "VisualEditor1",
+        "Effect" : "Allow",
+        "Action" : [
+          "secretsmanager:GetRandomPassword",
+          "secretsmanager:ListSecrets"
+        ],
+        "Resource" : "*"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role" "allow-secret-manager-read-access" {
+  name = "Roboshop-RabbitMQ-SecretManager-ReadAccess-${var.ENV}"
+
+  # Terraform's "jsonencode" function converts a
+  # Terraform expression result to valid JSON syntax.
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      },
+    ]
+  })
+
+  tags = {
+    tag-key = "Roboshop-RabbitMQ-SecretManager-ReadAccess-${var.ENV}"
+  }
+}
+
+resource "aws_iam_group_policy_attachment" "attach-policy" {
+  group      = aws_iam_role.allow-secret-manager-read-access.name
+  policy_arn = aws_iam_policy.allow-secret-manager-read-access.arn
+}
+
+resource "aws_iam_instance_profile" "allow-secret-manager-read-access" {
+  name = "test_profile"
+  role = aws_iam_role.allow-secret-manager-read-access.name
+}
--- security-group.tf
+++ security-group.tf
@@ -2,12 +2,21 @@ resource "aws_security_group" "main" {
   name        = "${local.TAG_PREFIX}-sg"
   description = "${local.TAG_PREFIX}-sg"
   vpc_id      = var.VPC_ID
+
   ingress {
     description      = "RABBITMQ"
     from_port        = var.PORT
     to_port          = var.PORT
     protocol         = "TCP"
     cidr_blocks      = var.ALLOW_SG_CIDR
+  }
+
+  ingress {
+    description      = "SSH"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "TCP"
+    cidr_blocks      = [var.WORKSTATION_IP]
   }
 
   egress {
